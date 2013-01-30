9070875399c612c19ff4822707df7f406411fc00
Osama <oaj09@ic.ac.uk>
2013-01-30 23:38:03 +0000

    commiting parser

--- src/parser.c
+++ src/parser.c
@@ -4,5 +4,27 @@
 
 
 struct Game* loadGame(  const char *filename ){
+  size_t lineSize = 1000;
+  char* line = (char*) malloc( sizeof(char)*lineSize);
+  FILE *file = fopen ( filename, "r" );
+  
+
+  if (file != NULL){
+    int cread;
+    cread = getline(&line,&lineSize,file);
+    while (cread >= 0 ){
+      printf("%s",line);
+      cread = getline(&line,&lineSize,file);
+    }
+				   
+  }
 
 }
+
+
+
+struct Node* parseNode( const char* line){
+  
+}
+
+
